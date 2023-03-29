84e5361e9bb6051ffe6f7554b49c194460199025
DuyunovNikita <119659583+DuyunovNikita@users.noreply.github.com>
2023-03-29 12:57:53 +0300

    Add files via upload

---
+++ Chat/src/main/java/org/example/AuthService.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public interface AuthService {
+
+    String getNick(String login, String pass);
+
+    boolean login(String login, String pass);
+
+    boolean contains(String userName);
+}
